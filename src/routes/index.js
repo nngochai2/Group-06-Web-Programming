c3a0b8cdc7415fa8396d3b7fc045db205998adcf
haritapcode <s3978281@rmit.edu.vn>
2023-09-13 14:50:00 +0700

    Updating the index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
-const express = require('express');
-const app = express();
+// Import the express library to create an Express.js router.
+// Express routers can mainain a cleaner and well-structured codebase.
+import express from 'express';
+// import { Product }
 
-// Tell Express to use EJS
-app.request('view engine', 'ejs');
-app.use
+
+// Create an Express router.
+export const indexRouter = express.Router();
