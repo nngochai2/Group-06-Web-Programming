0907796e0d58bba30097676fd074a4a064ba7d25
haritapcode <s3978281@rmit.edu.vn>
2023-09-15 17:16:34 +0700

    set up packages

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(logger);
+
+app.get("/", (req, res) => {
+    console.log("Here");
+    res.render(index)
+})
+
+
+
+
+// Setup middleware
+function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next()
+}
+
+app.listen(5000);
