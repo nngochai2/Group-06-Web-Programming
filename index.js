e55fbf9054e6a3db85d33852e958854bdba5f3bd
MikeyNhan <143437655+vietnhan0207@users.noreply.github.com>
2023-09-21 20:27:05 +0700

    Add my account

--- index.js
+++ index.js
@@ -12,29 +12,17 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.render('frontpage'); 
 });
-app.get('/login', (req, res) => {
-  res.render('login'); 
-});
-app.get('/login/register', (req, res) => {
-  res.render('user'); 
-});
-app.get('/login/register/customer_registration_form', (req, res) => {
-  res.render('customer_register'); 
-});
-app.get('/login/register/vendor_registration_form', (req, res) => {
-  res.render('vendor_register'); 
-});
-app.get('/login/register/shipper_registration_form', (req, res) => {
-  res.render('shipper_register'); 
-});
 app.get('/shopping', (req, res) => {
-  res.render('customer');
+  res.render('customer'); 
 });
 app.get('/products', (req, res) => {
   res.render('products'); 
 });
 app.get('/cart', (req, res) => {
   res.render('cart'); 
+});
+app.get('/myaccount', (req, res) => {
+  res.render('myaccount'); 
 });
 
 app.listen(port, () => {
