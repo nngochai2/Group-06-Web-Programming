38ed2ff05a2908ba902a522ba2a2c5c11552adea
nekormit <131735813+nekormit@users.noreply.github.com>
2023-09-21 20:35:17 +0700

    Delete src directory

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-// Import the express library to create an Express.js router.
-// Express routers can mainain a cleaner and well-structured codebase.
-import express from 'express';
-// import { Product }
-
-
-// Create an Express router.
-export const indexRouter = express.Router();
