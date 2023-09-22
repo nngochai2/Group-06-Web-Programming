8e7991216c8fd5493fa2375a26a35183f721aa64
Andy Nguyen <ndnguyen.ad@gmail.com>
2023-09-22 22:42:19 +0700

    Remove redundancies

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.set('views','/src/views/pages/index.ejs');
-app.set("view engine", "ejs");
-
-app.listen(PORT)
