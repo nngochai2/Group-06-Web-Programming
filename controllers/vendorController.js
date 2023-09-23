0f87237823bccc4e79499ff29682b46932d59aa2
Andy Nguyen <ndnguyen.ad@gmail.com>
2023-09-23 21:37:30 +0700

    vendor fix

--- controllers/vendorController.js
+++ controllers/vendorController.js
@@ -3,14 +3,19 @@ const Order = require('../models/order');
 
 exports.viewProducts = async (req, res) => {
   try {
-    const vendorId = req.user._id; 
-    const products = await Product.find({ vendorId });
-    res.render('vendor', { products }); // Changed the view name to 'vendor' to match the EJS file name
+    // Check if the user is logged in
+    const vendorId = req.user ? req.user._id : null;
+    
+    // If vendorId is null, you can either show all products or no products depending on your requirements
+    const products = vendorId ? await Product.find({ vendorId }) : [];
+    
+    res.render('vendor', { products });
   } catch (error) {
     console.error('Error viewing products:', error);
     res.status(500).send('Internal Server Error');
   }
 };
+
 
 exports.addProduct = async (req, res) => {
   try {
@@ -19,7 +24,7 @@ exports.addProduct = async (req, res) => {
     if (name.length < 10 || name.length > 20) {
         return res.status(400).send('Product name must be between 10 and 20 characters');
     }
-    if (description.length > 500) {
+    if (description.length < 500) {
         return res.status(400).send('Description must be at most 500 characters');
     }
     const newProduct = new Product({
--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,12 +1,8 @@
 const express = require('express');
+const router = express.Router();
 const vendorController = require('../controllers/vendorController');
 
-const router = express.Router();
-
-router.get('/view-products', vendorController.viewProducts);
-router.get('/add-product', (req, res) => {
-    res.render('add-product');
-});
+router.get('/', vendorController.viewProducts);
 router.post('/add-product', vendorController.addProduct);
 
 module.exports = router;
